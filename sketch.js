d9f23d3d01b087abc38c67451be9fe8fdc914afa
Ashwin67-MSD <73777149+Ashwin67-MSD@users.noreply.github.com>
2021-04-19 14:39:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+function preload(){
+  No1=loadImage("images/car1.png")
+  No2=loadImage("images/car2.png")
+  No3=loadImage("images/car3.png")
+  No4=loadImage("images/car4.png")
+  road=loadImage("images/track.jpg")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState===2){
+    game.end()
+  }
+}
+
